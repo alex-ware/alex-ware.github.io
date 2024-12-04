472b59500bdcb642e8ae81bc7692dfcadc14c9e1
alex.menzieadh@gmail.com <alex.menzieadh@gmail.com>
2024-12-04 14:09:51 +1100

    remove js test

--- formats.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const header = document.getElementById("header");
-  const headerHeight = header.offsetHeight;
-  document.documentElement.style.setProperty(
-    "--header-height",
-    `${headerHeight}px`
-  );
-});
